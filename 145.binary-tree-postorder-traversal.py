66c0a247f31b42f275a9be0b083f0a22acbf0ff9
Yingbo <zyb12269205@gmail.com>
2020-06-20 08:14:03 +0000

    update 145.binary-tree-postorder-traversal.py

---
+++ 145.binary-tree-postorder-traversal.py
@@ -0,0 +1,59 @@
+#
+# @lc app=leetcode id=145 lang=python3
+#
+# [145] Binary Tree Postorder Traversal
+#
+# https://leetcode.com/problems/binary-tree-postorder-traversal/description/
+#
+# algorithms
+# Hard (54.10%)
+# Likes:    1656
+# Dislikes: 86
+# Total Accepted:    370.9K
+# Total Submissions: 685.1K
+# Testcase Example:  '[1,null,2,3]'
+#
+# Given a binary tree, return the postorder traversal of its nodes' values.
+#
+# Example:
+#
+#
+# Input: [1,null,2,3]
+# ⁠  1
+# ⁠   \
+# ⁠    2
+# ⁠   /
+# ⁠  3
+#
+# Output: [3,2,1]
+#
+#
+# Follow up: Recursive solution is trivial, could you do it iteratively?
+#
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def postorderTraversal(self, root: TreeNode) -> List[int]:
+        if root is None:
+            return []
+        q = [root,]
+        rst = []
+        while q:
+            t = q.pop()
+            if t.left:
+                q.append(t.left)
+            if t.right:
+                q.append(t.right)
+            rst.append(t.val)
+        return rst[::-1]
+
+
+
+# @lc code=end
